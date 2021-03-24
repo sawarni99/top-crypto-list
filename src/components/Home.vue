08ce87402ff627c0fe419b2e8bf8d78d32c34110
Sawarni Swaroop <sawarni99@gmail.com>
2021-03-24 11:59:19 +0530

    Added pagination

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="home">
+    <h3 class="header">Top Crypto Currency List</h3>
+    <div class="home-body">
+      <div v-for="item in pageOfItems" :key="item.id">{{ item.name }}</div>
+    </div>
+    <div class="pagination">
+      <jw-pagination
+        :pageSize="20"
+        :items="listCurrency"
+        @changePage="onChangePage"
+      ></jw-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,15 +23,28 @@ export default {
   name: "Home",
   data() {
     return {
-      list: [],
+      listCurrency: [],
+      pageOfItems: [],
     };
   },
   mounted() {
     Vue.axios
       .get("https://api.coinranking.com/v1/public/coins/?limit=100")
       .then((resp) => {
-        this.list = resp.data.data.coin;
+        console.log(resp.data.data.coins);
+        this.listCurrency = resp.data.data.coins;
       });
+  },
+  methods: {
+    onChangePage(pageOfItems) {
+      this.pageOfItems = pageOfItems;
+    },
   },
 };
 </script>
+
+<style>
+.home {
+  font-family: "Roboto", sans-serif;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import JwPagination from "jw-vue-pagination";
+Vue.component("jw-pagination", JwPagination);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
