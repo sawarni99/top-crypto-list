0f5b539f70f58a78f4a873fe48a6724e011e1600
Sawarni Swaroop <sawarni99@gmail.com>
2021-03-24 11:29:24 +0530

    new commit

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
+Vue.use(VueAxios, axios);
+export default {
+  name: "Home",
+  data() {
+    return {
+      list: [],
+    };
+  },
+  mounted() {
+    Vue.axios
+      .get("https://api.coinranking.com/v1/public/coins/?limit=100")
+      .then((resp) => {
+        this.list = resp.data.data.coin;
+      });
+  },
+};
+</script>
